924a8597eb3d23621c2c222de9c347529d9f0c77
Darwin Risser <riser.darwin@gmail.com>
2019-04-06 11:34:46 -0500

    moved header

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const app = express();
 app.use(bodyParser.json());
 const UserRoutes = require("./routes/UserRoutes");
 
-app.use(UserRoutes);
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+app.use(UserRoutes);
+
 
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
