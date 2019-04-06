0c0a77feebfdc36b9ab8a82785e6221b7a7c34a7
Darwin Risser <riser.darwin@gmail.com>
2019-04-05 23:04:53 -0500

    trying cors headers

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.use(bodyParser.json());
 const UserRoutes = require("./routes/UserRoutes");
 
 app.use(UserRoutes);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.listen(process.env.PORT || 3000, (err) => {
   if (err) {
